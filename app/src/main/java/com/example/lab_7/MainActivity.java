866ede177b41a3f9f87c02510968317355f8da23
jatin-kumar-soni <learn.explore2024@gmail.com>
2025-09-22 14:30:52 +0530

    This is my first Commit

---
+++ app/src/main/java/com/example/lab_7/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.lab_7;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.chaquo.python.Python;
+import com.chaquo.python.PyObject;
+import com.chaquo.python.android.AndroidPlatform;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Start Python
+        if (!Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+
+        // Get Python instance
+        Python py = Python.getInstance();
+
+        // Load script.py file from assets or main/python
+        PyObject module = py.getModule("script");
+
+        // Call main() function inside script.py
+        PyObject num = module.callAttr("main");
+
+        // Just to check result in Logcat
+        System.out.println("Python says: " + num.toString());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
